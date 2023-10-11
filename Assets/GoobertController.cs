a8f9f58d87033f94b8aa6c2881cc5bb6ac575d32
Oliver Granudden <oliver.granudden@elev.ga.ntig.se>
2023-10-11 15:54:41 +0200

    initial

---
+++ Assets/GoobertController.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+
+public class GoobertController : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 5;
+
+    [SerializeField]
+    float force = 5;
+
+    [SerializeField]
+    LayerMask groundLayer;
+
+    [SerializeField]
+    float groundRadius = 0.2f;
+
+    Rigidbody2D rigidBody;
+
+    bool hasReleasedJump;
+
+    Vector2 footPosition;
+
+    Vector2 bottomColliderSize = Vector2.Zero;
+
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+
+        float moveX = Input.GetAxisRaw("Horizontal");
+
+        Vector2 movement = new Vector2(moveX, 0) * speed * Time.deltaTime;
+
+        transform.Translate(movement);
+
+        footPosition = transform.position;
+
+        // bool isGrounded = Physics2D.OverlapCircle(footPosition, groundRadius, groundLayer);
+        bool isGrounded = Physics2D.OverlapBox(GetFootPos(), GetFootSize(), 0);
+
+        if (Input.GetAxisRaw("Jump") > 0 && hasReleasedJump && isGrounded)
+        {
+            rigidBody.AddForce(Vector2.up * force, ForceMode2D.Impulse);
+            hasReleasedJump = false;
+        }
+
+        if(Input.GetAxisRaw("Jump") == 0)
+        {
+            hasReleasedJump = true;
+        }
+    }
+
+
+
+    private Vector2 GetFootPos()
+    {
+        float height = GetComponent<Collider2D>().bounds.size.y;
+        return transform.position + Vector3.down * height/2;
+    }
+
+    private Vector2 GetFootSize()
+    {
+        return new Vector2(GetComponent<Collider2D>().bounds.size.x *0.9f, 0.1);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+
+        Gizmos.DrawWireSphere(GetFootPos(), GetFootSize());
+    
+    }
+}
