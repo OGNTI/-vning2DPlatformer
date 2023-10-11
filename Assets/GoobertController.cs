4f67952e702a55a1efbc001558066a2403ac5379
Oliver Granudden <oliver.granudden@elev.ga.ntig.se>
2023-10-11 16:24:15 +0200

    gsfg

--- Assets/GoobertController.cs
+++ Assets/GoobertController.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Numerics;
 using UnityEngine;
 
 
@@ -24,7 +23,7 @@ public class GoobertController : MonoBehaviour
 
     Vector2 footPosition;
 
-    Vector2 bottomColliderSize = Vector2.Zero;
+    Vector2 bottomColliderSize = Vector2.zero;
 
     void Awake()
     {
@@ -51,7 +50,7 @@ public class GoobertController : MonoBehaviour
             hasReleasedJump = false;
         }
 
-        if(Input.GetAxisRaw("Jump") == 0)
+        if (Input.GetAxisRaw("Jump") == 0)
         {
             hasReleasedJump = true;
         }
@@ -62,18 +61,16 @@ public class GoobertController : MonoBehaviour
     private Vector2 GetFootPos()
     {
         float height = GetComponent<Collider2D>().bounds.size.y;
-        return transform.position + Vector3.down * height/2;
+        return transform.position + Vector3.down * height / 2;
     }
 
     private Vector2 GetFootSize()
     {
-        return new Vector2(GetComponent<Collider2D>().bounds.size.x *0.9f, 0.1);
+        return new Vector2(GetComponent<Collider2D>().bounds.size.x * 0.9f, 0.1f);
     }
 
     private void OnDrawGizmosSelected()
     {
-
-        Gizmos.DrawWireSphere(GetFootPos(), GetFootSize());
-    
+        Gizmos.DrawWireCube(GetFootPos(), GetFootSize());
     }
 }
